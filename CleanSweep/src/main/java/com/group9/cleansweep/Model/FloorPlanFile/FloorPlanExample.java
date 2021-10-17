fd7273876893cf7eea36d010e7281959c20f4be9
asayu <asayu2019@icloud.com>
2021-10-17 15:35:24 -0500

    create example file and read it as String[][]

---
+++ CleanSweep/src/main/java/com/group9/cleansweep/Model/FloorPlanFile/FloorPlanExample.java
@@ -0,0 +1,8 @@
+package com.group9.cleansweep.Model.FloorPlanFile;/**
+* 
+* @Description:
+* @Author: asayu
+* @Creat:10/12/21 3:08 PM
+*
+**/public class FloorPlanExample {
+}
---
+++ CleanSweep/src/main/java/com/group9/cleansweep/Model/FloorPlanFile/ReadFloorPlanFile.java
@@ -0,0 +1,8 @@
+package com.group9.cleansweep.Model.FloorPlanFile;/**
+* 
+* @Description:
+* @Author: asayu
+* @Creat:10/17/21 1:53 PM
+*
+**/public class ReadFloorPlanFile {
+}
