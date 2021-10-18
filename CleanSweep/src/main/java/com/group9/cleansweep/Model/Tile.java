f26d7dfc8e09941684d1868e0241926ea4f00644
MichaelKotwica <45274996+MichaelKotwica@users.noreply.github.com>
2021-10-17 23:01:00 -0500

    Added Tile.java, base object for cleansweep
    contains:
    
    dirtAmount, isChargingStation, visited, leftNext, rightNext, topNext, bottomNext, surfaceType

---
+++ CleanSweep/src/main/java/com/group9/cleansweep/Model/Tile.java
@@ -0,0 +1,28 @@
+package cleanSweep;
+
+public class Tile {
+	
+	public String surfaceType;
+	public int dirtAmount;
+	
+	boolean isChargingStation;
+	
+	boolean visited;
+	
+	public Tile rightNext;
+	public Tile leftNext;
+	public Tile topNext;
+	public Tile bottomNext;
+	
+	Tile(String S) {
+		this.surfaceType = S;
+		this.leftNext = null;
+		this.rightNext = null;
+		this.topNext = null;
+		this.bottomNext = null;
+		this.dirtAmount = 0;
+		this.isChargingStation = false;
+		this.visited = false;
+	}
+
+}
