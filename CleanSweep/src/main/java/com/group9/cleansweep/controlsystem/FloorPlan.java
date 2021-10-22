14cef7c37f8e3949330b969e1cb84d386f13a82e
joezaksheske <joseph.zaksheske@gmail.com>
2021-10-21 21:20:07 -0500

    added comments and added charging station to generic floorplan

--- CleanSweep/src/main/java/com/group9/cleansweep/controlsystem/FloorPlan.java
+++ CleanSweep/src/main/java/com/group9/cleansweep/controlsystem/FloorPlan.java
@@ -9,9 +9,8 @@ import com.group9.cleansweep.Enum.FloorPlanTypeEnum;
 import lombok.Getter;
 
 public class FloorPlan {
-	//this keeps track of all the tiles in a room
+	//this keeps track of all the tiles in a room String is the ID of the tile
 	private final Map<String, Tile> roomLayout;
-	public ArrayList<Tile> layout;
 	private Map<String, Boolean> tileVisitedMap;
 	private final String[] floortypes = {"BARE_FOOT", "LOW_PILE_CARPET", "HIGH_PILE_CARPET"};
 	private final Boolean[] isObstacle = {true, false};
@@ -21,7 +20,6 @@ public class FloorPlan {
 
 	public FloorPlan(){
 		this.roomLayout = new HashMap<>();
-		this.layout = new ArrayList<>();
 		buildGenericFloorPlan();
 		System.out.println("something");
 	}
@@ -46,17 +44,21 @@ public class FloorPlan {
 	public void buildGenericFloorPlan(){
 		Random random = new Random();
 		String[] alpha = {"a", "b", "c", "d", "e", "f", "g"};
+		//these loops create the tiles and add them to the map
 		for(int i = 0; i < 7; i++){
 			String letter = alpha[i];
 			for(int j = 1; j <= 7; j++ ){
 				Tile tempTile = new Tile();
+				//setting tile to random floor type declared at top of class
 				tempTile.setSurfaceType(floortypes[random.nextInt(floortypes.length)]);
+				//tile is randomly an obstacle or not
 				tempTile.setIsObstacle(isObstacle[random.nextInt(isObstacle.length)]);
 				String tempID = letter + j;
 				tempTile.setID(tempID);
 				roomLayout.put(tempTile.id, tempTile);
 			}
 		}
+		//these loops go and attempt to get all the tiles in all directions catches the out of bounds and ignores it
 		for(int z = 0; z < 7; z++){
 			Tile tempTile;
 			String letter = alpha[z];
@@ -103,6 +105,9 @@ public class FloorPlan {
 				}
 			}
 		}
+		//get tile g3 in order to make it the the charging station
+		Tile chargingStation = roomLayout.get("g3");
+		chargingStation.setChargingStation(true);
 	}
 
 	public void removeRoom(String roomID){
