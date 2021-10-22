669c9241e3b4d7280b3eb36bc30bbf58983c3256
joezaksheske <joseph.zaksheske@gmail.com>
2021-10-21 21:14:08 -0500

    added logic to make basic floorplan when constructing the class

--- CleanSweep/src/main/java/com/group9/cleansweep/controlsystem/FloorPlan.java
+++ CleanSweep/src/main/java/com/group9/cleansweep/controlsystem/FloorPlan.java
@@ -2,10 +2,7 @@ package com.group9.cleansweep.controlsystem;
 
 //import netscape.javascript.JSObject;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 import com.group9.cleansweep.Enum.FloorPlanTypeEnum;
 
@@ -13,31 +10,99 @@ import lombok.Getter;
 
 public class FloorPlan {
 	//this keeps track of all the tiles in a room
-	private final Map<String, ArrayList<String>> roomLayout;
+	private final Map<String, Tile> roomLayout;
+	public ArrayList<Tile> layout;
 	private Map<String, Boolean> tileVisitedMap;
+	private final String[] floortypes = {"BARE_FOOT", "LOW_PILE_CARPET", "HIGH_PILE_CARPET"};
+	private final Boolean[] isObstacle = {true, false};
 
 	@Getter
 	FloorPlanTypeEnum floorPlanType;
 
 	public FloorPlan(){
 		this.roomLayout = new HashMap<>();
+		this.layout = new ArrayList<>();
+		buildGenericFloorPlan();
+		System.out.println("something");
 	}
 
-	public ArrayList<String> getRoomTiles(String roomID){
-		return roomLayout.get(roomID);
-	}
+//	public ArrayList<String> getRoomTiles(String roomID){
+//		return roomLayout.get(roomID);
+//	}
 
-	public void addTileToRoom(String roomID, String tile){
-		ArrayList<String> tempList = roomLayout.get(roomID);
-		tempList.add(tile);
-		roomLayout.put(roomID, tempList);
-	}
+//	public void addTileToRoom(String roomID, String tile){
+//		ArrayList<String> tempList = roomLayout.get(roomID);
+//		tempList.add(tile);
+//		roomLayout.put(roomID, tempList);
+//	}
 
-	public String addNewRoom(){
-		String roomID = UUID.randomUUID().toString();
-		ArrayList<String> roomTiles = new ArrayList<>();
-		roomLayout.put(roomID, roomTiles);
-		return roomID;
+//	public String addNewRoom(){
+//		String roomID = UUID.randomUUID().toString();
+//		ArrayList<String> roomTiles = new ArrayList<>();
+//		roomLayout.put(roomID, roomTiles);
+//		return roomID;
+//	}
+
+	public void buildGenericFloorPlan(){
+		Random random = new Random();
+		String[] alpha = {"a", "b", "c", "d", "e", "f", "g"};
+		for(int i = 0; i < 7; i++){
+			String letter = alpha[i];
+			for(int j = 1; j <= 7; j++ ){
+				Tile tempTile = new Tile();
+				tempTile.setSurfaceType(floortypes[random.nextInt(floortypes.length)]);
+				tempTile.setIsObstacle(isObstacle[random.nextInt(isObstacle.length)]);
+				String tempID = letter + j;
+				tempTile.setID(tempID);
+				roomLayout.put(tempTile.id, tempTile);
+			}
+		}
+		for(int z = 0; z < 7; z++){
+			Tile tempTile;
+			String letter = alpha[z];
+			for(int x = 1; x <= 7; x++ ){
+				//try getting the top tile above target
+				try{
+					String targetTile = letter + x;
+					tempTile = roomLayout.get(targetTile);
+					String tileAbove = alpha[z-1] + x;
+					Tile upTile = roomLayout.get(tileAbove);
+					tempTile.setTopNext(upTile);
+				} catch(Exception e){
+					//ignore
+				}
+				//try getting the bottom tile above target
+				try{
+					String targetTile = letter + x;
+					tempTile = roomLayout.get(targetTile);
+					String tileBelow = alpha[z+1] + x;
+					Tile bottomTile = roomLayout.get(tileBelow);
+					tempTile.setBottomNext(bottomTile);
+				} catch(Exception e){
+					//ignore
+				}
+				//try getting the right tile above target
+				try{
+					String targetTile = letter + x;
+					tempTile = roomLayout.get(targetTile);
+					String tileRight = letter + (x+1);
+					Tile rightTile = roomLayout.get(tileRight);
+					tempTile.setRightNext(rightTile);
+				} catch(Exception e){
+					//ignore
+				}
+				//try getting the left tile above target
+				try{
+					String targetTile = letter + x;
+					tempTile = roomLayout.get(targetTile);
+					String tileLeft = letter + (x-1);
+					Tile leftTile = roomLayout.get(tileLeft);
+					tempTile.setLeftNext(leftTile);
+				} catch(Exception e){
+					//ignore
+				}
+			}
+		}
 	}
 
 	public void removeRoom(String roomID){
--- CleanSweep/src/main/java/com/group9/cleansweep/controlsystem/Tile.java
+++ CleanSweep/src/main/java/com/group9/cleansweep/controlsystem/Tile.java
@@ -13,17 +13,59 @@ public class Tile {
 	public Tile leftNext;
 	public Tile topNext;
 	public Tile bottomNext;
+	public String id;
+	public Boolean isObstacle;
 	
-	Tile(String S) {
-		this.surfaceType = S;
+	Tile() {
+		this.id = null;
+		this.surfaceType = null;
 		this.leftNext = null;
 		this.rightNext = null;
 		this.topNext = null;
 		this.bottomNext = null;
 		this.dirtAmount = 0;
 		this.isChargingStation = false;
+		this.isObstacle = false;
 		this.visited = false;
 	}
+
+	public void setSurfaceType(String surfaceType){
+		this.surfaceType = surfaceType;
+	}
+
+	public void setLeftNext(Tile leftNext){
+		this.leftNext = leftNext;
+	}
+
+	public void setRightNext(Tile rightTile){
+		rightNext = rightTile;
+	}
+
+	public void setTopNext(Tile topNext){
+		this.topNext = topNext;
+	}
+
+	public void setBottomNext(Tile bottomNext){
+		this.bottomNext = bottomNext;
+	}
+
+	public void setDirtAmount(int dirtAmount){
+		this.dirtAmount = dirtAmount;
+	}
+
+	public void setChargingStation(Boolean isChargingStation){
+		this.isChargingStation = isChargingStation;
+	}
+
+	public void setIsObstacle(Boolean isObstacle){
+		this.isObstacle = isObstacle;
+	}
+
+	public void setID(String id){
+		this.id = id;
+	}
+
+
 
 
 
