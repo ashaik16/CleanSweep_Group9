9107cd450f0de30c24031544b02c202f37e9700d
joezaksheske <joseph.zaksheske@gmail.com>
2021-10-31 15:28:32 -0500

    added logic for traversal

--- CleanSweep/src/main/java/com/group9/cleansweep/controlsystem/CleanSweep.java
+++ CleanSweep/src/main/java/com/group9/cleansweep/controlsystem/CleanSweep.java
@@ -1,8 +1,5 @@
 package com.group9.cleansweep.controlsystem;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public class CleanSweep {
 
 	/*
@@ -14,28 +11,18 @@ public class CleanSweep {
 	 * 
 	 */
 
-	@Getter
-	private Long id;
+	public CleanSweep(){
 
-	@Getter
-	@Setter
-	FloorPlan floorPlan;
-	@Getter
-	@Setter
-	ObstacleDetection obstacleDetection;
-	@Getter
-	@Setter
-	DirtDetection dirtDetection;
-	@Getter
-	@Setter
-	Navigation navigation;
-	@Getter
-	@Setter
-	PowerManagement powerManagement;
+
+	}
 
 	public void doWork() {
 		FloorPlan floorPlan = new FloorPlan();
 		floorPlan.buildGenericFloorPlan();
+		Navigation navigation = new Navigation(floorPlan);
+		DirtDetection dirtDetection = new DirtDetection();
+		Tile startingPoint = navigation.getFirstTile();
+		Tile nextTile = navigation.getNextTile(startingPoint);
 		dirtDetectionProcess(floorPlan);
 		floorPlan.writeFloorPlanToFile();
 	}
@@ -43,11 +30,13 @@ public class CleanSweep {
 	public void doWorkFromFile(String fileLocation){
 		FloorPlan floorPlan = new FloorPlan();
 		floorPlan.convertFileToFloorPlan("src/main/java/com/group9/cleansweep/controlsystem/FloorPlanFile/SampleFloor.json");
+		Navigation navigation = new Navigation(floorPlan);
+		DirtDetection dirtDetection = new DirtDetection();
 		dirtDetectionProcess(floorPlan);
 	}
 
 	public void dirtDetectionProcess(FloorPlan floorPlan) {
-		DirtDetection dirtDetecting = new DirtDetection();
-		dirtDetecting.dirtDetectionProcess(floorPlan);
+		DirtDetection dirtDetection = new DirtDetection();
+		dirtDetection.dirtDetectionProcess(floorPlan);
 	}
 }
--- CleanSweep/src/main/java/com/group9/cleansweep/controlsystem/Navigation.java
+++ CleanSweep/src/main/java/com/group9/cleansweep/controlsystem/Navigation.java
@@ -3,9 +3,14 @@ package com.group9.cleansweep.controlsystem;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.LinkedList;
+import java.util.Locale;
+
 public class Navigation {
 	// this is where the stack/queue would be for tiles that have been visited
 	// need a method that returns boolean for is cleaning done
+	private LinkedList<Tile> visitedTiles;
+	private FloorPlan floorPlan;
 	@Getter
 	@Setter
 	private boolean isVisited = false;
@@ -20,11 +25,65 @@ public class Navigation {
 	//print moved left one cell
 	//return left Tile object
 
+	public Navigation(FloorPlan floorPlan){
+		visitedTiles = new LinkedList<>();
+		this.floorPlan = floorPlan;
+	}
+
 	public boolean isCleaningComplete(){
 		return isTrackingCycleComplete();
 	}
 
-	public Tile getFirstTile(FloorPlan floorPlan){
+	public Tile getFirstTile(){
 		return floorPlan.getFirstTile();
+	}
+
+	public Tile getNextTile(Tile currentTile){
+		visitedTiles.add(currentTile);
+		if(canMoveToNextTile(currentTile, "UP")){
+			System.out.println("Moving up one tile");
+			return floorPlan.getTile(currentTile.getTopNext());
+		} else if( canMoveToNextTile(currentTile, "RIGHT")){
+			System.out.println("Moving right one tile");
+			return floorPlan.getTile(currentTile.getRightNext());
+		} else if(canMoveToNextTile(currentTile, "DOWN")){
+			System.out.println("Moving down right tile");
+			return floorPlan.getTile(currentTile.getBottomNext());
+		} else if(canMoveToNextTile(currentTile, "LEFT")){
+			System.out.println("Moving down left tile");
+			return floorPlan.getTile(currentTile.getLeftNext());
+		}
+		return currentTile;
+	}
+
+	private Boolean canMoveToNextTile(Tile currentTile, String direction){
+		Tile targetTile;
+		try{
+			switch(direction.toUpperCase(Locale.ROOT)) {
+				case "UP":
+					targetTile = floorPlan.getTile(currentTile.getTopNext());
+					if(!visitedTiles.contains(targetTile) && !targetTile.getObstacle()){
+						return true;
+					}
+				case "DOWN":
+					targetTile = floorPlan.getTile(currentTile.getBottomNext());
+					if(!visitedTiles.contains(targetTile) && !targetTile.getObstacle()){
+						return true;
+					}
+				case "RIGHT":
+					targetTile = floorPlan.getTile(currentTile.getRightNext());
+					if(!visitedTiles.contains(targetTile) && !targetTile.getObstacle()){
+						return true;
+					}
+				case "LEFT":
+					targetTile = floorPlan.getTile(currentTile.getLeftNext());
+					if(!visitedTiles.contains(targetTile) && !targetTile.getObstacle()){
+						return true;
+					}
+			}
+		} catch (Exception e){
+			return false;
+		}
+		return false;
 	}
 }
