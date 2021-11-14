048d761ca97b4b89a8188c17e0632d33b8612b74
MDolphin259 <ldolphin259@gmail.com>
2021-11-14 08:46:32 -0600

    Deleted the unused sensor files

--- control-system/src/main/java/com/group9/cleansweep/controlsystem/LeftSensor.java
+++
@@ -1,9 +0,0 @@
-package com.group9.cleansweep.controlsystem;
-
-public class LeftSensor {
-
-    public ObstacleDetection.ObstacleType run() {
-        ObstacleDetection obstacle = new ObstacleDetection();
-        return obstacle.isObstacleDetected;
-    }
-}
